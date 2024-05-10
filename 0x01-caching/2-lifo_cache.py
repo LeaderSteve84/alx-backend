d5ae378ec4c25a47d24733239d0353a04fc92093
stephen_adah <mailstephenreal@gmail.com>
2024-05-10 19:50:25 +0100

    task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""FIFO cache module"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO Cache"""
+
+    def __init__(self):
+        """object constructor"""
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """Assigne a key to a value"""
+        if key is None or item is None:
+            return
+
+        if key not in self.stack:
+            self.stack.append(key)
+        else:
+            self.migrate_to_last(key)
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard_d = self.stack.pop()
+            del self.cache_data[discard_d]
+            print("DISCARD: {}". format(discard_d))
+
+    def get(self, key):
+        """returns value in self.cache_dat linked to key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+
+        return self.cache_data.get(key)
+
+    def migrate_to_last(self, key):
+        """move an element"""
+        if self.stack[-1] != key:
+            self.stack.remove(key)
+            self.stack.append(key)
