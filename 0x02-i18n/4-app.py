5b29af5ba7a2d857fd9333bccc694e2fd6ac66a7
stephen_adah <mailstephenreal@gmail.com>
2024-05-15 16:47:18 +0100

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""simple flask app outputs “Welcome to Holberton” as
+page title (<title>) and “Hello world”
+as header (<h1>)
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """class for the configuration for babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get_locale function"""
+    if request.args.get('locale'):
+        lanG = request.args.get('locale')
+        if lanG in app.config['LANGUAGES']:
+            return lanG
+        else:
+            return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home_route() -> str:
+    """route to home page"""
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
